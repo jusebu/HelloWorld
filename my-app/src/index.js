ef9db93951730fbcbcb4bb7dde2ec662dd26fa01
Juan Sebastián Bustamante <jusebu@hotmail.com>
2021-10-03 22:05:24 -0500

    Tabla Ventas 2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
+//import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-mg9b1vqp.us.auth0.com"
-      clientId="STT2ZTvCS2wWAtClC7xOzCoMrgenNVcd"
-      redirectUri = "{window.location.origin}">
+    
     <App />
-      </Auth0Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
